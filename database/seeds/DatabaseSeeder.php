dce76742e79b7987f9ac7a84f2f18f63981084c8
Jered Masters <jered.masters@outlook.com>
2018-04-11 23:00:31 +0800

    added duration variance

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,22 +16,24 @@ class DatabaseSeeder extends Seeder
     foreach([4, 8] as $mutation_rate){
       foreach([2, 4] as $selection_pressure){
         foreach([0, 1] as $gradient_decent){
-          foreach([0, 1] as $duration_variance){
-            foreach([0, 1, 2] as $mutation_variance){
-              for ($pop_size = 20; $pop_size <= 200; $pop_size += 20) {
-                for ($gens = 20; $gens <= 200; $gens += 20) {
-                  $t = [
-                    "population" => $pop_size,
-                    "generations" => $gens,     
-                    "selection_pressure" => $selection_pressure,
-                    "duration" => 100 * 120,
-                    "crossover_rate" => 6,
-                    "mutation_rate" => $mutation_rate,
-                    "mutation_variance" => $mutation_variance,
-                    "duration_variance" => $duration_variance,
-                    "gradient_decent" => $gradient_decent
-                  ];
-                  $this->createTest($t);
+          foreach([100 * 100, 100 * 150, 100 * 200] as $duration){
+            foreach([0, 1] as $duration_variance){
+              foreach([0, 1, 2] as $mutation_variance){
+                for ($pop_size = 20; $pop_size <= 200; $pop_size += 20) {
+                  for ($gens = 20; $gens <= 200; $gens += 20) {
+                    $t = [
+                      "population" => $pop_size,
+                      "generations" => $gens,     
+                      "selection_pressure" => $selection_pressure,
+                      "duration" => 100 * 120,
+                      "crossover_rate" => 6,
+                      "mutation_rate" => $mutation_rate,
+                      "mutation_variance" => $mutation_variance,
+                      "duration_variance" => $duration_variance,
+                      "gradient_decent" => $gradient_decent
+                    ];
+                    $this->createTest($t);
+                  }
                 }
               }
             }
