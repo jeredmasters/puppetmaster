44e11d91c92f4ad301fc3997edd2063d4c219a8b
Jered Masters <jered.masters@outlook.com>
2018-10-08 22:38:22 +0800

    fix duration variance vs runtime

--- app/Services/Analysis.php
+++ app/Services/Analysis.php
@@ -391,8 +391,8 @@ class Analysis
         'sets' => [
           ['label' => 'Off', 'filter' => ['duration_variance' => 0, 'steepest_descent' => 0]],
           ['label' => 'On', 'filter' => ['duration_variance' => 1, 'steepest_descent' => 0]],
-          ['label' => 'Off SD', 'filter' => ['duration_variance' => 0, 'steepest_descent' => 0]],
-          ['label' => 'On SD', 'filter' => ['duration_variance' => 1, 'steepest_descent' => 0]],
+          ['label' => 'Off SD', 'filter' => ['duration_variance' => 0, 'steepest_descent' => 1]],
+          ['label' => 'On SD', 'filter' => ['duration_variance' => 1, 'steepest_descent' => 1]],
         ],
         'x' => [
           'label' => 'Generations',
